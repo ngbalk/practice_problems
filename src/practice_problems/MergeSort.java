723216a7ebb29083dd7f7c7fbe7114798bc7f741
Nick Balkissoon <nickbalkissoon@Nicks-MacBook-Pro.local>
2014-10-02 12:54:28 -0400

    implemented cleaner merge sort

--- src/practice_problems/MergeSort.java
+++ src/practice_problems/MergeSort.java
@@ -1,57 +1,55 @@
 package practice_problems;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MergeSort {
+	/**
+	 * Writing divideAndConquer to implement a mergeSort.  divideAndConquer is a recursive method.  Only requires 4 lines of code.  
+	 * call conquer which takes two parameters, each of which will only return when the list is of length 1, then it returns and we start to pop
+	 * back up the stack.
+	 * @param list
+	 * @return
+	 */
 	
-	public static ArrayList<Integer> sort(ArrayList<Integer> list){
-		int length = list.size();
-		if(length == 1){
+	public static List<Integer> divideAndConquer(List<Integer> list){
+		System.out.println(list.toString());
+		if(list.size()==1){
 			return list;
 		}
-		return merge(sort(cut(list, 0, list.size()/2 - 1)), sort(cut(list, list.size() / 2, list.size() - 1)));
+		return conquer(divideAndConquer(list.subList(0, list.size()%2 == 0 ? list.size()/2 : list.size()/2 + 1)), divideAndConquer(list.subList(list.size()%2 == 0 ? list.size()/2 : list.size()/2 + 1, list.size())));
+
+		
+		
+
 	}
-	public static ArrayList<Integer> cut(ArrayList<Integer> list, Integer begin, Integer end){
-		ArrayList<Integer> ret = new ArrayList<Integer>();
-		for(int i = begin; i<=end; i ++){
-				ret.add(list.get(i));
-		}
-		return ret;
-	}
-	public static ArrayList<Integer> doSort(ArrayList<Integer> toSort){
-		return sort(toSort);
-	}
-	
-	
-	public static ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
-		ArrayList<Integer> ret = new ArrayList<Integer>();
-		int aInd = 0;
-		int bInd = 0;
-		while(aInd<=a.size() && bInd<=b.size()){
+	public static List<Integer> conquer(List<Integer> a, List<Integer> b){
+		List<Integer> ret = new ArrayList<Integer>();
+		int aInd=0;
+		int bInd=0;
+		while(true){
 			if(aInd==a.size()){
-				ret.addAll(bInd, b);
+				ret.addAll(b.subList(bInd, b.size()));
 				break;
 			}
-			else if(bInd==b.size()){
-				ret.addAll(aInd, a);
+			if(bInd==b.size()){
+				ret.addAll(a.subList(aInd, a.size()));
 				break;
 			}
-			else if(a.get(aInd) <= b.get(bInd)){
+			if(a.get(aInd)<=b.get(bInd)){
 				ret.add(a.get(aInd));
-				aInd ++;
-				continue;
+				aInd++;
 			}
-			else if(b.get(bInd) <= a.get(aInd)){
+			else{
 				ret.add(b.get(bInd));
-				bInd ++;
-				continue;
-				
+				bInd++;
 			}
 		}
-			
 		return ret;
 		
 	}
+	
+	
 	
 	public static void main(String[] args){
 		Integer[] testData = {5, 24, 2, 1, 55, 12, 14, 12, 31, 9, 100, 26, 83, 3};
@@ -59,6 +57,6 @@ public class MergeSort {
 		for(Integer i : testData){
 			testArray.add(i);
 		}
-		System.out.println(doSort(testArray).toString());
+		System.out.println(divideAndConquer(testArray).toString());
 	}
 }
